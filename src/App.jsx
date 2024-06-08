46ab5b77e349352a5b28e0c33c2ee4b559e79a1d
Samridda Paudel <84239594+samriddaxdev@users.noreply.github.com>
2024-06-08 06:47:38 +0545

    using async function to load on to the API of NASA to view the star

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Main from './components/Main';
 import SideBar from './components/SideBar';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
   function handleToggleModal() {
@@ -15,22 +17,43 @@ function App() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
       const url =
         'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`;
-    }
-    try {
-      const res =await fetch(url)
-      const data = await res.json()
-      console.log('DATA\n', data)
 
-    } catch (err) {
-      console.log(err.message)
+      const today = new Date().toDateString();
+      const localKey = `NASA-${today}`;
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey));
+        setData(apiData);
+        console.log('Fetched from cache today');
+        return;
+      }
+      localStorage.clear();
+
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        localStorage.setItem(localKey, JSON.stringify(apiData));
+        setData(apiData);
+        console.log('Fetched from API today');
+      } catch (err) {
+        console.log(err.message);
+      }
     }
-    fetchAPIData()
+    fetchAPIData();
   }, []);
+
   return (
     <>
-      <Main />
-      {showModal && <SideBar handleToggleModal={handleToggleModal} />}
-      <Footer handleToggleModal={handleToggleModal} />
+      {data ? (
+        <Main data={data} />
+      ) : (
+        <div className='loadingState'>
+          <i className='fa-solid fa-gear'></i>
+        </div>
+      )}
+      {showModal && (
+        <SideBar data={data} handleToggleModal={handleToggleModal} />
+      )}
+      {data && <Footer data={data} handleToggleModal={handleToggleModal} />}
     </>
   );
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,43 +1,13 @@
 export default function SideBar(props) {
-  const { handleToggleModal } = props;
+  const { handleToggleModal, data } = props;
   return (
     <div className='sidebar'>
       <div onClick={handleToggleModal} className='bgOverlay'></div>
       <div className='sidebarContents'>
-        <h2>The brutal Martian Landscape</h2>
-        <div>
-          <p>Description</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
-            lacinia turpis. Duis quis sagittis dui, a interdum augue. Fusce
-            facilisis nisi ex, eu consectetur est tincidunt non. Nam tempus in
-            elit ut suscipit. In hac habitasse platea dictumst. Mauris
-            ullamcorper mattis nisl, et congue nisl elementum vitae. Phasellus
-            fermentum pharetra ex, et molestie erat dapibus in. Donec enim
-            dolor, commodo vel facilisis quis, vestibulum ac tortor. Fusce
-            convallis massa quis viverra tristique. Nunc blandit imperdiet
-            tincidunt. Aliquam varius pretium turpis, et eleifend nunc hendrerit
-            sed. Donec ullamcorper, metus vel pretium ultricies, lacus purus
-            blandit quam, non pretium massa erat ut ligula. Curabitur massa
-            purus, condimentum a mattis nec, condimentum ac augue. Nam non
-            tortor felis. Nunc dictum dolor metus, vitae commodo sapien
-            imperdiet lobortis. Suspendisse viverra, massa in vestibulum
-            lacinia, nulla nulla pretium augue, eu fringilla augue felis quis
-            tortor. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. Praesent vestibulum imperdiet est
-            ac condimentum. Suspendisse gravida, magna vitae iaculis blandit,
-            nulla odio ornare diam, ut varius mauris ex nec orci. Sed eu dui
-            urna. Nullam ornare eu ligula vel pulvinar. Integer neque velit,
-            sollicitudin eget tincidunt eget, consequat in urna. Etiam iaculis
-            placerat arcu nec sagittis. Phasellus ex velit, laoreet vel tellus
-            a, ultricies lacinia augue. Sed vel gravida ligula. Nullam sagittis
-            vestibulum dolor, non molestie turpis scelerisque ac. Mauris pretium
-            quam sem, ac ornare urna accumsan vel. Phasellus lacinia nunc odio,
-            nec congue libero ultrices vel. Etiam ante augue, convallis
-            ullamcorper dolor eu, maximus pellentesque augue. Donec felis nibh,
-            congue eu nulla at, finibus egestas diam. Proin dolor odio, feugiat
-            quis aliquet quis, porta a est.
-          </p>
+        <h2>{data?.title}</h2>
+        <div className='descriptionContainer'>
+          <p className='descriptionTitle'>{data?.date}</p>
+          <p>{data?.explanation}</p>
         </div>
         <button onClick={handleToggleModal}>
           <i className='fa-solid fa-arrow-right'></i>
